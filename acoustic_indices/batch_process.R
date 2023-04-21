93a13b94109bd55e5b1d32307ee58cf2a15703a9
Gio Jacuzzi <giojacuzzi@gmail.com>
2023-04-20 18:09:09 -0700

    additional diagnostics file

--- acoustic_indices/batch_process.R
+++ acoustic_indices/batch_process.R
@@ -4,38 +4,45 @@ library(tuneR)
 library(soundecology)
 library(seewave)
 
-# input_files - a list of paths to .wav files
-# output_path - path to the output directory
-# output_file - name of the resulting output file (without .csv extension)
-# alpha_indices - indices to calculate, e.g. c('BIO','ADI')
-# time_interval - sampling interval length per calculation (sec)
-# ncores - number of cores to use
-# dc_correct - correct dc bias offset prior to index calculation
-# digits - number of decimal places for results
+# input_files     - a list of paths to .wav files
+# output_path     - path to the output directory
+# output_file     - name of the generated output file (without .csv extension)
+# alpha_indices   - indices to calculate, e.g. c('BIO','ADI')
+# time_interval   - sampling interval length per calculation (sec)
+# ncores          - number of cores to use
+# dc_correct      - correct dc bias offset prior to index calculation
+# digits          - number of decimal places for results
+# diagnostics     - generate an additional file with information about the input file(s)
 #
-# This process will create a .csv file with the following fields:
-# File - the path and name of the input file
-# SamplingRate - the sampling rate of the file (Hz)
-# BitDepth - the bit depth of the file
-# Start - the start time of the time interval processed
-# End - the end time of the time interval processed (if no interval specified, then this is the entire length of the file)
-# DcBias - the mean DC bias amplitude of the file (before applying any DC correction)
-# Clipping - whether any clipping was detected
+# This process will generate <output_file>.csv with the following fields:
+# File      - the path and name of the input file
+# Start     - the start time of the time interval processed
+# End       - the end time of the time interval processed (if no interval specified, then this is the entire length of the file)
 # ACI, etc. - the calculated values for each associated acoustic index
+#
+# It will also generate <output_file>_diagnostics.csv, if specified:
+# SamplingRate - the sampling rate of the file (Hz)
+# BitDepth     - the bit depth of the file
+# Duration     - the duration of the file (sec)
+# DcBias       - the mean DC bias amplitude of the file (before applying any DC correction)
+# Clipping     - whether any clipping was detected
 # 
 # NOTE: only mono .wav support
 # NOTE: ACI will return NA for long duration files (perhaps >= 10 min + @ 32kHz),
 # this is an issue with the 'soundecology' package
-batch_process = function(
-    input_files, output_path, output_file = 'batch_process', alpha_indices = c(), time_interval = NA, ncores = 1, dc_correct = T, digits = 4, ...) {
+batch_process_alpha_indices = function(
+    input_files, output_path, output_file = 'batch', alpha_indices = c(), time_interval = NA, ncores = 1, dc_correct = T, digits = 4, diagnostics = T, ...) {
+
   if (length(alpha_indices) == 0) stop('Specify at least one alpha acoustic index')
 
   message(' Starting batch process (', paste(alpha_indices,collapse=' '),')')
     
-  file_header = c('File,Start,End,ACI,ADI,AEI,BIO,H,M,NDSI')
-  # TODO: File,SamplingRate,BitDepth,Duration,DcBias,Clipping
+  output_header = c('File,Start,End,ACI,ADI,AEI,BIO,H,M,NDSI')
+  diagnostics_header = c('File,SamplingRate,BitDepth,Duration,DcBias,Clipping')
   
   ## Internal function to calculate indices
+  # Returns a list where element 1 is the acoustic index result data in csv form,
+  # and element 2 is the diagnostic data (if requested)
   calculate_alpha_indices = function(file, clustered = F, ...) {
     source('helpers.R')
     
@@ -120,8 +127,18 @@ batch_process = function(
     }
     
     # Calculate file diagnostics
-    dc_bias  = mean(wav@left)
-    clipping = clipping(wav)
+    if (diagnostics) {
+      results_diagnostics = paste0(
+        '\n', file,',',                   # File (including path)
+        wav@samp.rate,',',                # SamplingRate (Hz)
+        wav@bit,',',                      # BitDepth
+        duration,',',                     # Duration (sec)
+        round(mean(wav@left),digits),',', # DcBias (mean amplitude value)
+        clipping(wav)                     # Clipping (boolean)
+      )
+    } else {
+      results_diagnostics = NA
+    }
     
     # Correct DC offset bias
     if (dc_correct) wav@left = dc_correct(wav@left)
@@ -202,12 +219,8 @@ batch_process = function(
       results = paste0(
         results,
         '\n', file,',',              # File (including path)
-        # wav_interval@samp.rate,',',  # SamplingRate (Hz)
-        # wav_interval@bit,',',        # BitDepth
         start,',',                   # Start time (sec)
         end,',',                     # End time (sec)
-        # round(dc_bias,digits),',',   # DcBias (mean amplitude value)
-        # clipping,',',                # Clipping (boolean)
         round(ACI,digits),',',       # acoustic complexity index
         round(ADI,digits),',',       # acoustic diversity index
         round(AEI,digits),',',       # acoustic evenness index
@@ -219,7 +232,7 @@ batch_process = function(
       
       start = end # move to next time interval
     }
-    return(list('results' = results))
+    return(list('results' = results, 'diagnostics' = results_diagnostics))
   }
   
   no_input_files = length(input_files)
@@ -231,7 +244,11 @@ batch_process = function(
   ## Start processing
   time_start = proc.time() # start timer
   output = paste0(output_path, output_file, '.csv', collapse = '')
-  cat(file_header, file = output, append = F) # open results file
+  cat(output_header, file = output, append = F) # open results file
+  if (diagnostics) {
+    output_diagnostics = paste0(output_path, output_file, '_diagnostics.csv', collapse = '')
+    cat(diagnostics_header, file = output_diagnostics, append = F) # open diagnostics file
+  }
   
   if (ncores > 1) { # Process in parallel using clusters
     
@@ -239,7 +256,14 @@ batch_process = function(
     
     cluster = makeCluster(ncores, type = 'PSOCK')
     results = parLapply(cluster, input_files, calculate_alpha_indices, clustered = T, ...)
-    write.table(paste(unlist(results), collapse = ''), file = output, append = T, quote = F, col.names = F, row.names = F)
+
+    # Write results to file(s)
+    write.table(paste(unlist(sapply(results,"[[",1)), collapse = ''),
+                file = output, append = T, quote = F, col.names = F, row.names = F)
+    if (diagnostics) {
+      write.table(paste(unlist(sapply(results,"[[",2)), collapse = ''),
+                  file = output_diagnostics, append = T, quote = F, col.names = F, row.names = F)
+    }
     Sys.sleep(1) # pause to allow all to end
     
     stopCluster(cluster)
@@ -249,25 +273,28 @@ batch_process = function(
     message(paste0(' Processing ', no_input_files, ' file(s) in series using 1 core'))
     for (file in input_files) {
       results = calculate_alpha_indices(file, ...)
+
+      # Write results to file(s)
       cat(results$results, file = output, append = T)
+      if (diagnostics) cat(results$diagnostics, file = output_diagnostics, append = T)
     }
   }
   
   time_end = proc.time() - time_start # stop timer
-  message(paste0(' Created ', output, '\n Total time: ', round(time_end['elapsed'], 2), ' sec'))
+  message(paste0(' Created ', output))
+  if (diagnostics) message(paste0(' Created ', output_diagnostics))
+  message(paste0(' Total time: ', round(time_end['elapsed'], 2), ' sec'))
 }
 
-# TEST #########
-# Params
-path = '~/../../Volumes/SAFS Work/DNR/test/subset'
-input_files = list.files(path=path, pattern='*.wav', full.names=T, recursive=F)
-# input_files = paste0(path, '/SMA00351_20210502_050002.wav')
-output_path = '~/../../Volumes/SAFS Work/DNR/test/subset/output/'
-alpha_indices = c('BIO', 'ACI')
-# Series
-# batch_process(input_files, output_path, alpha_indices = alpha_indices, time_interval = 60*2, ncores = 1)
-# Parallel
-batch_process(input_files, output_path, alpha_indices = alpha_indices, time_interval = 60*2, ncores = 3)
-# Other
-# batch_process(input_files, output_path, alpha_indices = c('BIO','AEI'), ncores = 3, min_freq = 200, max_freq = 2000, db_threshold = -45)
-##############
+# # EXAMPLE #########
+# # Params
+# path = '~/../../Volumes/SAFS Work/DNR/test/subset'
+# input_files = list.files(path=path, pattern='*.wav', full.names=T, recursive=F)
+# output_path = '~/../../Volumes/SAFS Work/DNR/test/subset/output/'
+# alpha_indices = c('ADI','BIO', 'ACI')
+# # Series
+# # batch_process_alpha_indices(input_files, output_path, alpha_indices = alpha_indices, time_interval = 60*2, ncores = 1)
+# # Parallel
+# batch_process_alpha_indices(input_files, output_path, alpha_indices = alpha_indices, time_interval = 60*2, ncores = 2, min_freq = 2000, max_freq = 8000, db_threshold = -40)
+# # Other
+# # batch_process_alpha_indices(input_files, output_path, alpha_indices = c('BIO','AEI'), ncores = 3, min_freq = 200, max_freq = 2000, db_threshold = -45)
