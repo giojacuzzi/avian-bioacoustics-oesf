0fa6713da4e21bc1ff9e15a7a2c449688d14c79d
Gio Jacuzzi <giojacuzzi@gmail.com>
2023-07-27 17:48:45 -0700

    directory parallel processing

--- classification/birdnet_parallel.py
+++ classification/birdnet_parallel.py
@@ -5,11 +5,64 @@ from birdnetlib.batch import DirectoryMultiProcessingAnalyzer
 from birdnetlib.analyzer import Analyzer
 import datetime
 
+# Analyzer config
+lat=47.676786
+lon=-124.136721
+min_conf=0.5
+
+# File config
+in_filetype = '.wav'
+in_dir = "D:\\DNR\\2021\\Deployment1_2021April13_14\\S4A04325_20210414_Data"
+# in_dir = 'C:\\Users\\gioj\\Desktop\\test'
+out_dir = os.path.dirname(__file__) + '\\_output\\'
+out_file = 'detections.csv'
+out_path = out_dir + out_file
+
+# Scrape serial number, date, and time from Song Meter filename
+def get_info_from_filename(path):
+    filename = os.path.basename(path)
+    substrs = filename.split(in_filetype)[0].split('_')
+    date = substrs[1]
+    time = substrs[2]
+    return({
+        'serial_no': substrs[0],
+        'date':      date,
+        'year':      date[0:4],
+        'month':     date[4:6],
+        'day':       date[6:8],
+        'time':      time,
+        'hour':      time[0:2],
+        'min':       time[2:4],
+        'sec':       time[4:6],
+    })
+
 def on_analyze_directory_complete(recordings):
-        print(f'Directory complete ({len(recordings)} recordings analyzed)')
-        for recording in recordings:
-            # print(recording.detections)
-            print('This is a recording!')
+    print(f'Directory complete ({len(recordings)} recordings analyzed)')
+
+    # Store detections in results
+    results = pd.DataFrame()
+    for recording in recordings:
+        result = pd.DataFrame(recording.detections)
+        print(f'{len(result)} detections from {recording.path}')
+        info = get_info_from_filename(recording.path)
+        print(f"{info['serial_no']} {info['year']}/{info['month']}/{info['day']} {info['hour']}:{info['min']}:{info['sec']}")
+
+        if not result.empty:
+            start_deltas = list(map(lambda s: datetime.timedelta(days=0, seconds=s), list(result['start_time'])))
+            start_dates = list(map(lambda d: dt + d, start_deltas))
+            result['start_date'] = start_dates
+
+            end_deltas = list(map(lambda s: datetime.timedelta(days=0, seconds=s), list(result['end_time'])))
+            end_dates = list(map(lambda d: dt + d, end_deltas))
+            result['end_date'] = end_dates
+
+            result['serial_no'] = info['serial_no']
+            result['file'] = os.path.basename(recording.path)
+
+            results = pd.concat([results, result], ignore_index=True)
+    
+    # Save to file
+    pd.DataFrame.to_csv(results, out_dir + str(os.path.basename(os.path.dirname(recordings[0].path))) + '.csv', index=False)
 
 def getDirectoriesWithFiles(path, filetype):
     directoryList = []
@@ -27,41 +80,30 @@ def getDirectoriesWithFiles(path, filetype):
 if __name__ == '__main__':
     analyzer = Analyzer()
 
-    # File config
-    in_filetype = '.wav'
-    out_dir = os.path.dirname(__file__) + '\\_output\\'
-    out_file = 'detections.csv'
-    out_path = out_dir + out_file
-    # in_dir = "D:\\DNR\\2021\\Deployment1_2021April13_14\\S4A04325_20210414_Data"
-    in_dir = 'C:\\Users\\gioj\\Desktop\\test'
+    for dir in getDirectoriesWithFiles(in_dir, in_filetype):
 
-    # for dir in getDirectoriesWithFiles(in_dir, in_filetype):
-    #     if __name__ == '__main__':
-    #         print('Analyzing directory ' + dir + '...')
+        # TODO: if 'dir' is in _output, we have analyzed it before, and should skip it
 
-    #         batch = DirectoryMultiProcessingAnalyzer(
-    #             directory=dir,
-    #             analyzers = [analyzer],
-    #             patterns=['*' + in_filetype]
-    #         )
-    #         batch.on_analyze_directory_complete = on_analyze_directory_complete
+        print('Analyzing directory ' + dir + '...')
+        info = get_info_from_filename(os.path.basename(os.path.normpath(dir))) # note 'time' here is nonsense
+        print(info['serial_no'])
+        print(info['date'])
+        dt = datetime.datetime(int(info['year']), int(info['month']), int(info['day']))
+        batch = DirectoryMultiProcessingAnalyzer(
+            directory=dir,
+            analyzers = [analyzer],
+            date=dt,
+            lat=lat,
+            lon=lon,
+            min_conf=min_conf,
+            patterns=[('*' + in_filetype)]
+        )
+        batch.on_analyze_directory_complete = on_analyze_directory_complete
+        print('Processing directory ' + dir +'...')
+        batch.process()
+        print('Done!')
 
-    #         print('Processing...')
-    #         batch.process()
-    #         print('Done!')
-
-    # print('FINITO!')
-
-    dir = getDirectoriesWithFiles(in_dir, in_filetype)[0]
-    batch = DirectoryMultiProcessingAnalyzer(
-        directory=dir,
-        analyzers = [analyzer],
-        patterns=["*.wav"]
-    )
-    batch.on_analyze_directory_complete = on_analyze_directory_complete
-    print('Processing...')
-    batch.process()
-    print('Done!')
+    print('FINITO DIRECTERO!')
 
 # ##########
 
