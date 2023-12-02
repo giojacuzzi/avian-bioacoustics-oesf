0efbdb3720be5960dcab035f52a33ff83b2f6245
Gio Jacuzzi <giojacuzzi@gmail.com>
2023-12-02 12:30:46 -0800

    clustering demo via scikit-maad

---
+++ annotation/cluster.py
@@ -0,0 +1,76 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from maad import sound, features, rois
+from maad.spl import power2dBSPL
+from maad.util import power2dB, plot2d, format_features, overlay_rois
+
+# Load file as spectogram
+print('Loading file as spectrogram...')
+s, fs = sound.load('/Users/giojacuzzi/Library/CloudStorage/GoogleDrive-giojacuzzi@gmail.com/My Drive/Research/Projects/OESF/Annotation/Data/SMA00404_20230518/SMA00404_20230518_052101_SS.wav')
+s_filt = sound.select_bandwidth(s, fs, fcut=1000, forder=2, ftype='highpass')
+
+db_max=100  # used to define the range of the spectrogram
+Sxx, tn, fn, ext = sound.spectrogram(s_filt, fs, nperseg=1024, noverlap=512)
+Sxx_db = power2dB(Sxx, db_range=db_max) + db_max
+plot2d(Sxx_db, **{'extent':ext})
+
+print('Showing plot...')
+plt.show()
+
+# Find regions of interest
+print('Finding regions of interest...')
+Sxx_db_rmbg, _, _ = sound.remove_background(Sxx_db)
+Sxx_db_smooth = sound.smooth(Sxx_db_rmbg, std=1.2)
+im_mask = rois.create_mask(im=Sxx_db_smooth, mode_bin ='relative', bin_std=2, bin_per=0.25)
+im_rois, df_rois = rois.select_rois(im_mask, min_roi=50, max_roi=None)
+
+# Format ROIs and visualize the bounding box on the audio spectrogram.
+df_rois = format_features(df_rois, tn, fn)
+ax0, fig0 = overlay_rois(Sxx_db, df_rois, **{'vmin':0, 'vmax':60, 'extent':ext})
+
+print('Showing plot...')
+plt.show()
+
+# Compute acoustic features
+print('Computing acoustic features...')
+df_shape, params = features.shape_features(Sxx_db, resolution='low', rois=df_rois)
+df_centroid = features.centroid_features(Sxx_db, df_rois)
+
+# Get median frequency and normalize
+median_freq = fn[np.round(df_centroid.centroid_y).astype(int)]
+df_centroid['centroid_freq'] = median_freq/fn[-1]
+print(median_freq)
+
+# Reduce dimensionality of features
+print('Reducing dimensionality...')
+from sklearn.manifold import TSNE
+
+X = df_shape.loc[:,df_shape.columns.str.startswith('shp')]
+X = X.join(df_centroid.centroid_freq) # add column and normalize values
+
+tsne = TSNE(n_components=2, perplexity=12, init='pca', verbose=True)
+Y = tsne.fit_transform(X)
+
+fig, ax = plt.subplots()
+ax.scatter(Y[:,0], Y[:,1], c='gray', alpha=0.8)
+ax.set_xlabel('tsne dim 1')
+ax.set_ylabel('tsne dim 2')
+
+print('Showing plot...')
+plt.show()
+
+# Cluster ROIs into homogeneous groups
+from sklearn.cluster import DBSCAN
+cluster = DBSCAN(eps=5, min_samples=4).fit(Y)
+print('Number of soundtypes found:', np.unique(cluster.labels_).size)
+
+from maad.util import rand_cmap
+fig, ax = plt.subplots()
+ax.scatter(Y[:,0], Y[:,1], c=cluster.labels_, cmap=rand_cmap(5 , first_color_black=False), alpha=0.8)
+ax.set_xlabel('tsne dim 1')
+ax.set_ylabel('tsne dim 2')
+
+# Overlay bounding box on the original spectrogram
+df_rois['label'] = cluster.labels_.astype(str)
+ax0, fig0 = overlay_rois(Sxx_db, df_rois, **{'vmin':0, 'vmax':60, 'extent':ext})
+plt.show()
